83471dcb6f7b4a7c90dac01e8a9f66be8fa1b530
Emma Fitzgerald <Emma.P.Fitzgerald@students.ittralee.ie>
2020-11-02 11:07:29 +0000

    Updated animation and added objects to character

--- Assets/AnimatorScript.cs
+++ Assets/AnimatorScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimatorScript : MonoBehaviour
 {
     Animator model_animator;
+    float speed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +27,12 @@ public class AnimatorScript : MonoBehaviour
 
         if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && Input.GetKey(KeyCode.LeftShift))
         {
+            speed = 4f;
             model_animator.SetBool("isRunning", true);
         }
         else
         {
+            speed = 2f;
             model_animator.SetBool("isRunning", false);
         }
 
@@ -41,6 +45,15 @@ public class AnimatorScript : MonoBehaviour
             model_animator.SetBool("isJumping", false);
         }
 
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            model_animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            model_animator.SetBool("isAttacking", false);
+        }
+
         Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
@@ -48,5 +61,8 @@ public class AnimatorScript : MonoBehaviour
     {
         model_animator.SetFloat("Ver_X",x);
         model_animator.SetFloat("Hor_Y",y);
+
+        transform.position += (Vector3.forward * speed) * y * Time.deltaTime;
+        transform.position += (Vector3.right * speed) * x * Time.deltaTime;
     }
 }
